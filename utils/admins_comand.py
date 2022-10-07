0e859cbfc72b1626bf6c841fb1dc39a8502d5204
bozdag05 <81674921+bozdag05@users.noreply.github.com>
2022-10-07 17:55:25 +0300

    Update admins_comand.py

--- utils/admins_comand.py
+++ utils/admins_comand.py
@@ -1,10 +1,11 @@
 from aiogram import Dispatcher
 
 
-from data.config import GENERAL_ID
+from data.config import GENERAL_ID, admins
 
 
 async def on_startup_admin(dp: Dispatcher):
     text = 'Bot MGOTU started'
     print(text)
-    await dp.bot.send_message(chat_id=GENERAL_ID, text=text)
+    for admin in admins:
+        await dp.bot.send_message(chat_id=admin, text=text)
